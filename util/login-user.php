81355a28f24a6f15c5979da777b8c5b29343a0e4
Sebastian Safari <Sebastian.Safari@parc.com>
2018-01-30 08:49:52 -0800

    copy code

---
+++ util/login-user.php
@@ -0,0 +1,15 @@
+User logged in:
+<?php
+
+echo $_POST["nm"];
+echo "<br>"; 
+echo $_POST["eml"]; 
+echo "<br>"; 
+echo $_POST["usr"]; 
+echo "<br>"; 
+echo $_POST["bdy"]; 
+echo "<br>"; 
+echo $_POST["pswd"]; 
+
+
+?>
