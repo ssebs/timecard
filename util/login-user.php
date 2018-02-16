385e59e777166c099673ea0f20a276bc109451cc
Sebastian Safari <Sebastian.Safari@parc.com>
2018-02-16 10:46:42 -0800

    Password Matching

--- util/login-user.php
+++ util/login-user.php
@@ -1,15 +1,33 @@
 User logged in:
 <?php
+    require "../connect.php";
 
-echo $_POST["nm"];
-echo "<br>"; 
-echo $_POST["eml"]; 
-echo "<br>"; 
-echo $_POST["usr"]; 
-echo "<br>"; 
-echo $_POST["bdy"]; 
-echo "<br>"; 
-echo $_POST["pswd"]; 
+    $sql = "SELECT Users.User, Passwds.Pass FROM Users INNER JOIN Passwds ON Users.User = Passwds.User WHERE Users.User = '" . $_POST["usr"] . "'";
+    $result = $conn->query($sql);
 
+    echo $_POST["usr"]; 
+    echo "<br>Passwd: "; 
+    echo $_POST["pswd"]; 
+?>
+<br>
+<?php
+    if ($result->num_rows > 0) 
+    {
+        while($row = $result->fetch_assoc()) 
+        {
+            // echo "<br>row pass = " . $row["Pass"];
+            // echo "<br>post pswd = " . $_POST["pswd"];
+
+            if($row["Pass"] == $_POST["pswd"]) 
+            {
+                echo "<h3>PASSWORD MATCH</h3>";
+                
+            }
+        
+        }
+    } else 
+    {
+        echo "0 results";
+    }
 
 ?>
